fbde6a000d09d323988bd40978da9086175373d2
Samuel <samuel.volpato@pg.inatel.br>
2022-10-01 11:43:15 -0300

    feat: console log do formData

---
+++ .vscode/localstore/app.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formData = {
+
+    };
+    console.log({ formData });
+});
